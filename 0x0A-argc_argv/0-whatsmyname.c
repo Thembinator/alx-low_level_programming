0543ced70e5b1e95d065612968594f6c1c08c4b8
Thembi <Thembichaukethb@gmail.com>
2022-09-30 13:55:23 -0700

    a program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints its own name
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 always success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("program's name is: %s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * main - header for prototypes
+ *
+ */
+
+int _putchar(char);
+int main(int argc, char *argv[]);
+
+#endif /* MAIN_H */
