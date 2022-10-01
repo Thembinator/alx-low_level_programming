5eb4f3e7b612faa9d2636e329c36e4345c4d54ae
Thembi <Thembichaukethb@gmail.com>
2022-10-01 08:13:07 -0700

    prints all arguments it receives

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -9,7 +9,15 @@
 
 int main(int argc, char *argv[])
 {
-	while (argc--)
-	printf("%s\n", *argv++);
-	exit (EXIT_SUCCESS);
+	int count = 0;
+
+	if (argc > 0)
+{
+	while (count < argc)
+{
+	printf("%s\n", argv[count]);
+	count++;
+}
+}
+	return (0);
 }
