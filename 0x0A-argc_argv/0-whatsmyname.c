d29b33ddb2c1f3c5a366abeeb2086ee2f393a365
Thembi <Thembichaukethb@gmail.com>
2022-10-03 04:54:22 -0700

    prints its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - program that prints its own name
  * @argc: argument count
  * @argv: argument vector
- * Return: 0 always success
+ * Return: always 0 success
  */
 
 int main(int argc, char *argv[])
