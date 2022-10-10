ad214f0225c49e8aa7edef49c476b77d0c6bd523
Thembi <Thembichaukethb@gmail.com>
2022-10-10 09:52:32 -0700

    prints the name of the file it was compiled from,

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
  * main - prints the name of the file it was compiled from
- * @argc: argument count
- * @argv: argument vector
  * Return: 0 always success
  */
 
-int main(int argc, char *argv[])
+int main(void)
 {
-	(void) argc;
-	printf("%s", argv[0]);
+	printf("%s\n", __FILE__);
 	return (0);
 }
