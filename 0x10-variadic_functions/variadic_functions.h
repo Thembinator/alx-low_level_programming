4bd7e8898a0ab85ed6f86255188192ab2f1f1d58
Thembi <Thembichaukethb@gmail.com>
2022-10-13 14:13:46 -0700

    prints anything from integer to string

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,6 +5,7 @@
 #include <stdio.h>
 
 int _putchar(char);
+int main(void);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
