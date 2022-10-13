8c179da7b1793be0e3eab7cb258e4b55dc793b13
Thembi <Thembichaukethb@gmail.com>
2022-10-13 12:06:44 -0700

    a variadic function that determines the sum of all its parameters

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,19 +1,8 @@
-#ifndef _VARIADIC_FUNCTIONS_H_
-#define _VARIADIC_FUNCTIONS_H_
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 
 #include <stdarg.h>
-
-/**
- * struct print - print  type with corresponding print function
- * @t: print type
- * @f: print function
- */
-
-typedef struct print
-{
-	char *t;
-	void (*f)(va_list);
-}print_t;
+#include <stdio.h>
 
 int _putchar(char);
 int sum_them_all(const unsigned int n, ...);
@@ -21,4 +10,4 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
-#endif /* _VARIADIC_FUNCTIONS_H_ */
+#endif /* VARIADIC_FUNCTIONS_H */
